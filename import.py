e5e440800afeac39a8de65aecd9f4bffa5881bd2
muhidham17 <49118793+muhidham17@users.noreply.github.com>
2019-10-16 17:11:25 +0800

    Create import.py

---
+++ import.py
@@ -0,0 +1,17 @@
+import math
+
+def penjumlahan(bil1, bil2):
+  penjumlahan = bil1+bil2
+  return penjumlahan
+  
+def pengurangan(bil1, bil2):
+  pengurangan = bil1-bil2
+  return pengurangan
+  
+def pembagian(bil1, bil2):
+  pembagian = bil1/bil2
+  return pembagian
+  
+def perkalian(bil1,bil2):
+  perkalian = bil1*bil2
+  return perkalian
